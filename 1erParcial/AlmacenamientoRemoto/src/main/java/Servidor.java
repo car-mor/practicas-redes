35a8c056d5124189d7e1444f46d35633a5a75fc3
Vanessa Trejo <adriana_seyer@outlook.com>
2024-04-05 00:14:09 -1200

    ..

--- 1erParcial/AlmacenamientoRemoto/src/main/java/Servidor.java
+++ 1erParcial/AlmacenamientoRemoto/src/main/java/Servidor.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 import javax.swing.JFileChooser;
 /*
  * @authors Carlos Moreno y Vanessa Trejo
@@ -9,7 +10,9 @@ public class Servidor {
         try{
            ServerSocket s = new ServerSocket(1234); //asociacion al puerto 1234
             s.setOption(StandardSocketOptions.SO_REUSEADDR, true);
+            
             System.out.println("Servidor iniciado en el puerto "+s.getLocalPort()); //si se creó, va a imprimir el puerto al que está asociado, si está filtrado o bloqueado por firewall mandaría excepción
+            System.out.println("Esperando conexiones del cliente... ");
             for(;;){ //while(true)
                 Socket cl = s.accept(); //en cada interacción aceptamos cliente, y devuelve referencia de tipo socket.
                 System.out.println("Cliente conectado desde-> "+cl.getInetAddress()+":"+cl.getPort());//registro de quien fue ultimo que se conecto
@@ -17,8 +20,11 @@ public class Servidor {
                     DataInputStream dis = new DataInputStream(cl.getInputStream());
                     String directiva = dis.readUTF();
                     DataOutputStream dos = new DataOutputStream(cl.getOutputStream());
+                    
+                    //empieza switch
                     switch(directiva){
-                        case "list":
+                        
+                        case "list": //caso 1
                             String instr1="Seleccione la carpeta de donde desea enlistar los directorios";
                             dos.writeUTF(instr1);
                             dos.flush();
@@ -49,19 +55,31 @@ public class Servidor {
                                    permisos = permisos+"x";
                                 dos.writeUTF(permisos);
                                 dos.flush();
-                                dos.writeInt(listado.length);
+                                String Seleccionada = f.getName();
+                                dos.writeUTF(Seleccionada);
                                 dos.flush();
-                                   for(int x =0;x<listado.length;x++){
-                                       if (listado[x].isDirectory()) {
-                                           dos.writeUTF(listado[x].getName());
-                                           dos.flush();
-                                       }
-                                   }//for
+                                enlistarRemoto(f, dos, f.getName());
                                }//if
                             }
                             dos.close();
                             dis.close();
                             cl.close();
+                            break;
+                        
+                        case "rmdir": //caso 2
+                            eliminarRemoto(dis,dos,cl);   
+                            break;    
+                            
+                        case "mkdir": //caso 6
+                            crearRemoto(dis,dos,cl);   
+                            break;
+                        
+                        case "quit": //caso 7
+                            salirAplicacion(dis,dos,cl);
+                            dis.close();
+                            dos.close();
+                            cl.close();
+                            //s.close();
                             break;
                     }          
                 }catch(Exception e){
@@ -71,5 +89,129 @@ public class Servidor {
         }catch(Exception e){
             e.printStackTrace();
         }
+    }//main
+    
+//********************************************** EMPIEZAN MÉTODOS ******************************
+    //caso 1. enlistar remoto
+    private static void enlistarRemoto(File f, DataOutputStream dos, String ruta) throws IOException {
+    dos.writeUTF(ruta);
+    dos.flush();
+
+    File[] listado = f.listFiles();
+    if (listado != null) {
+        dos.writeInt(listado.length);
+        dos.flush();
+
+        for (File subdir : listado) {
+            if (subdir.isDirectory()) {
+                enlistarRemoto(subdir, dos, ruta + "/" + subdir.getName());
+            }
+        }
+    } else {
+        dos.writeInt(0);
+        dos.flush();
     }
-}
+    }
+    
+    //caso 2. eliminar archivos/directorios remoto
+    private static void eliminarRemoto(DataInputStream dis, DataOutputStream dos, Socket cl) {
+        try{
+            //se envia la instrucción al cliente
+            String instr="Seleccione la carpeta/archivo que desea eliminar. "
+                    + "NOTA. Si elimina una carpeta con archivos se eliminarán también los archivos";
+            dos.writeUTF(instr);
+            dos.flush();
+            
+        }catch (IOException e) {
+            e.printStackTrace();
+        }//try catch
+    }//termina metodo eliminar remoto
+    
+    //caso 6. crear dir. remoto
+    private static void crearRemoto(DataInputStream dis, DataOutputStream dos, Socket cl) {
+        try{   
+            //se envia la instrucción al cliente
+            String instrMKDIR="Seleccione la carpeta de donde desea crear el directorio";
+            dos.writeUTF(instrMKDIR);
+            dos.flush();
+                            
+            //se selecciona la carpeta remota
+            JFileChooser jfMKDIR = new JFileChooser();
+            File dirMKDIR = new File("d:\\Documentos\\");
+            jfMKDIR.setCurrentDirectory(dirMKDIR);
+            jfMKDIR.setRequestFocusEnabled(true);
+            jfMKDIR.requestFocus();
+            jfMKDIR.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+            int rMKDIR = jfMKDIR.showDialog(null, "Elegir");
+            if(rMKDIR==JFileChooser.APPROVE_OPTION){
+                File fMKDIR = jfMKDIR.getSelectedFile();
+                String tipoMKDIR = (fMKDIR.isDirectory())?"Carpeta":"Archivo";
+                String carpetaMKDIR = "\033[32m Elegiste: "+fMKDIR.getAbsolutePath();
+                                
+                //envia carpeta remota absolutePath
+                dos.writeUTF(carpetaMKDIR);
+                dos.flush();
+                String TipoMKDIR = "Tipo: "+tipoMKDIR;
+                                
+                //envia tipo
+                dos.writeUTF(TipoMKDIR);
+                dos.flush();
+                                
+                //envía permisos
+                if(tipoMKDIR.compareTo("Carpeta")==0){
+                    File[]listadoMKDIR = fMKDIR.listFiles();
+                    String permisosMKDIR="";
+                    if(fMKDIR.canRead())
+                        permisosMKDIR = permisosMKDIR+"r";
+                    if(fMKDIR.canWrite())
+                        permisosMKDIR = permisosMKDIR+"w";
+                    if(fMKDIR.canExecute())
+                        permisosMKDIR = permisosMKDIR+"x";
+                    dos.writeUTF(permisosMKDIR);
+                    dos.flush();
+                                
+                    //envia la segunda instruccion
+                    String instr2MKDIR="Ingresa el nombre del nuevo directorio: ";
+                    dos.writeUTF(instr2MKDIR);
+                    dos.flush();
+                                
+                    //recibe el nombre de la carpeta
+                    String entradaMKDIR = dis.readUTF();
+                                
+                    //se crea la nueva carpeta remota
+                    String pathMKDIR = fMKDIR.getAbsolutePath() + "\\" + entradaMKDIR;
+                    File f1MKDIR = new File(pathMKDIR);
+                    boolean boolMKDIR = f1MKDIR.mkdirs();  //crea la carpeta
+                    f1MKDIR.setWritable(true); //le damos permisos de escritura o habilitamos
+                    f1MKDIR.setReadable(true); // habilita los permisos de lectura
+                                
+                    //se envía al cliente si la carpeta fue creada correctamente o no
+                    if(boolMKDIR){  
+                        String instr3MKDIR="El directorio se creó satisfactoriamente ";
+                        dos.writeUTF(instr3MKDIR);
+                        dos.flush();  
+                    }else{ 
+                        String instr4MKDIR="Hubo un error al crear el directorio";
+                        dos.writeUTF(instr4MKDIR);
+                        dos.flush();
+                    } //if
+                }//if
+            }//if
+        }catch (IOException e) {
+            e.printStackTrace();
+        }//try catch
+    }//termina metodo crear remoto
+    
+    //caso 7. salir de la aplicacion ja
+    private static void salirAplicacion(DataInputStream dis, DataOutputStream dos, Socket cl) {
+        try{
+            //se envia la instrucción al cliente
+            String instr="Cerrando flujos de salida, entrada y socket server...";
+            dos.writeUTF(instr);
+            dos.flush();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }//try catch
+    }//termina metodo salir app
+      
+}//servidor
